e47993ef2ca55cd5438f36dc6c28c095d4fe1da6
faisalamin12 <faisalamin011@gmail.com>
2020-12-26 00:51:22 +0500

    Error Resolved

--- calculator_app/calculator_app/urls.py
+++ calculator_app/calculator_app/urls.py
@@ -19,6 +19,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("home/", views.home , name="home"),
-    path("home/", views.calculate , name = "calculate")
+    path("", views.home , name="home"),
+    path("calculate/", views.calculate , name = "calculate")
 ]
