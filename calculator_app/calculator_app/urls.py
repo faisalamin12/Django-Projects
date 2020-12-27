13bf3b2b25e2436d8f587b574671c7c8d2405687
faisalamin12 <faisalamin011@gmail.com>
2020-12-27 16:17:27 +0500

    Calculation Logic Compeleted
    Calculation Logic Completed Desigining Remaining (Incompelete)

--- calculator_app/calculator_app/urls.py
+++ calculator_app/calculator_app/urls.py
@@ -20,5 +20,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.home , name="home"),
-    path("calculate/", views.calculate , name = "calculate")
+    path("calculate", views.calculate , name = "calculate")
 ]
--- calculator_app/calculator_app/views.py
+++ calculator_app/calculator_app/views.py
@@ -9,15 +9,53 @@ def calculate(request):
      num1_input = request.GET.get("num1")
      op = request.GET.get("operator")
      num2_input = request.GET.get("num2")
-     result = num1_input + num2_input
 
-     inputs = {
-        "num1": num1_input,
-        "num2": num2_input,
-        "operator": op,
-        "Result":result
-    }
 
-     return render(request , "home.html", inputs)
+     if op == "+":
+         result = int(num1_input) + int(num2_input)
+
+         data = {
+             "Result": result
+         }
+
+         return render(request , "home.html",data)
+    
+    
+     elif op == "-":
+        result = int(num1_input) - int(num2_input)
+
+        data = {
+             "Result": result
+         }
+
+        return render(request , "home.html",data)
+        
+
+     elif op == "*":
+        result = int(num1_input) * int(num2_input)
+
+        data = {
+             "Result": result
+         }
+
+        return render(request , "home.html",data)
+   
+   
+     elif op == "/":
+        result = int(num1_input) / int(num2_input)
+
+        data = {
+             "Result": result
+         }
+
+        return render(request , "home.html",data)
+        
+
+
+
+
+
+
+     
 
 
